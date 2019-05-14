114889389ba84725939132342d9251e612461ea1
Ben Harosh <benha@jfrog.com>
2019-05-14 15:01:48 -0700

    webhook json parsing

--- bitbuckethook.py
+++ bitbuckethook.py
@@ -3,17 +3,27 @@ import json
 from buildbot.plugins import webhooks
 from buildbot.util import bytes2unicode
 
-class BitBucketHook(webhooks.base):
+class BitBuckethook(webhooks.base):
     def getChanges(self, request):
         payload = self._get_payload(request)
+        revision=payload['refChanges'][0]['toHash']
+
+        for changeset in payload['changesets']['values']:
+            if changeset['toCommit']['id'] ==  revision:
+                revlink=changeset['links']['self'][0]['href'].rstrip('#README.md')
+                author=changeset['toCommit']['author']['name']
+                message=changeset['toCommit']['message']
+
         chdict = dict(
-                      revision=payload['refChanges'][0]['toHash'],
+                      revision=revision,
                       repository=payload['repository']['name'],
-                      repo_url=payload['changesets']['values'][0]['links']['self'][0]['href'].rstrip('#README.md').rstrip('commits/{}'.format(payload['refChanges'][0]['toHash'])),
                       project=payload['repository']['project']['name'],
-                      author=payload['changesets']['values'][0]['toCommit']['author']['name'],
                       branch=payload['refChanges'][0]['refId'],
-                      revlink=payload['changesets']['values'][0]['links']['self'][0]['href'].rstrip('#README.md'))
+                      revlink=revlink,
+                      author=author,
+                      comments='Bitbucket Server Pull Request #{} .\nCommit comment: {}'.format(revision, message) 
+                      )
+        print('change payload data that was sent to db: {} '.format(chdict))             
         return ([chdict], None)
 
     def _get_payload(self, request):
